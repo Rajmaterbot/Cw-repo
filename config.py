c2234d9b10340baad28c7c1d1fab3ad586a0d6ea
HackerADbro <150064036+HackerADbro@users.noreply.github.com>
2023-11-20 20:11:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6818261867:AAFhXboqTtBzh7vl1Mh05B9z0VZZGHsC9iU")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6419414727:AAGqI6o9Di5CmzW1ikNdvrjp21dDFEMrb88")
     API_ID = int(os.environ["API_ID", 23442389]
     API_HASH = os.environ["API_HASH", "70490ec8a810932cb5cb7f9d6a839ee0"]
     AUTH_USERS = "6693536228 """
